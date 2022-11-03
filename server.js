ac553cad490ad156ab82461de7f0faf1db4b5530
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-11-02 21:40:33 -0300

    feat: server responding to tweet post request and get request

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 import express from "express";
-import bodyParser from "body-parser" // usado para pegar o objeto que vem do post;
-import cors from "cors"
+import bodyParser from "body-parser"; // usado para pegar o objeto que vem do post;
+import cors from "cors";
 
-let userInfo;
+let userInfo = [];
+let tweetsVector = [];
 
-const app =express();
-app.use(cors())
-app.use(bodyParser.json()) // ativa o uso de bodyParser.json() no servidor;
-app.post("/sign-up",(req,resp)=>{
-    console.log(req.body)
-  resp.send('Ok')
-})
+const app = express();
+app.use(cors());
+app.use(bodyParser.json()); // ativa o uso de bodyParser.json() no servidor;
+
+app.post("/sign-up", (req, resp) => {
+  userInfo.push(req.body);
+  resp.send("Ok");
+});
+
+app.get("/tweets", (req, res) => {
+  res.send(tweetsVector);
+});
+
+app.post("/tweets", (req, res) => {
+  console.log(req.body);
+  const postOwner = userInfo.find(
+    (element) => element.username === req.body.username
+  );
+  const avatarOwner = postOwner.avatar;
+  tweetsVector.push({ ...req.body, avatar: avatarOwner });
+  res.send("Ok");
+});
 
 app.listen(5000);
